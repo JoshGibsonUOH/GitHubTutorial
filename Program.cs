005f91a32e51a6bed0455729534884a2b79d03dc
JoshGibsonUOH <119119908+JoshGibsonUOH@users.noreply.github.com>
2023-11-21 11:20:01 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine("Hello! " + name);
+}
